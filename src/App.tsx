e64b58a9b82634d784aead00547e89b065994795
Colin Harrington <colin.harrington@gmail.com>
2020-07-02 14:21:02 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import "./styles.css";
+
+export default function App() {
+  // Structure the code however you'd like.
+  // Display a search box with a submit button.
+  // When a search term is entered and the submit button is clicked:
+  // Fetch movies whose title matches the search term from the open movie database (http://www.omdbapi.com/) using API Key 60f47463
+  // If you encounter problems with the provided API key, please register for your own.
+  // Display each of the results in a list/table that should show (any order/format - be creative!):
+  // 1. The movie title
+  // 2. The image/poster for the movie
+  // 3. The MPAA rating
+  // 4. The runtime length
+  // 5. The Rotten Tomatoes score, if available.
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
